810c695ed31e80df396e349a1ab8b682b95690ee
Bogdyans <BOGDYA2004@gmail.com>
2025-03-17 21:35:10 +0300

    fix(header): make header look more like in figma file

--- src/components/mobile/header.tsx
+++ src/components/mobile/header.tsx
@@ -1,23 +1,17 @@
 import {Bell, Search} from "lucide-react";
+import Image from "next/image";
 
 
 export default function Header(){
     return (
-        <div className="flex justify-between items-center px-4 py-3 border-b border-[#f5f5f5]">
-            <Search className="w-6 h-6"/>
+        <header className="flex justify-between items-center px-4 py-3 border-b border-[#f5f5f5] ">
+            <Image src="/media/icons/search.png" alt="search" width={33} height={33} />
             <div className="flex flex-col items-center">
-                <div className="relative w-32 h-8">
-                    <div className="absolute w-full h-4 top-0">
-                        <div className="relative w-full h-full">
-                            <div className="absolute w-full h-1 bg-[#e30613] rounded-full top-1"></div>
-                            <div
-                                className="absolute w-3 h-3 bg-[#e30613] rounded-full top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/3"></div>
-                        </div>
-                    </div>
-                    <div className="absolute bottom-0 w-full text-center font-bold tracking-widest">ПРОМТЕХ</div>
+                <div className="relative w-32 h-12">
+                     <Image src="/media/logo.png" alt="logo" width={151} height={58} />
                 </div>
             </div>
-            <Bell className="w-6 h-6"/>
-        </div>
+            <Image src="/media/icons/notifications.png" alt="notifications" width={33} height={33} />
+        </header>
     )
 }
