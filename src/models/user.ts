ee186c288d4854dafdb5dc661660ea6935186350
Bogdyans <BOGDYA2004@gmail.com>
2025-04-21 20:46:37 +0300

    format(auth controller): restructure auth controller for cleaner code architecture

---
+++ src/models/user.ts
@@ -0,0 +1,34 @@
+import { PoolClient } from "pg";
+
+
+
+export default class UserModel {
+  static async findById(client: PoolClient, userId: number) {
+    const query = `
+    SELECT 
+      u.id, u.login, u.role_id, u.created_at, u.last_login_at, u.is_active,
+      up.fio, up.email, up.phone_number, up.job_title, up.otdel_id, up.location, up.pseudonim
+    FROM "User" u
+    LEFT JOIN "userprofile" up ON u.id = up.user_id
+    WHERE u.id = $1
+  `;
+    const result = await client.query(query, [userId]);
+    return result.rows[0] ?? null;
+  }
+
+  static async findByUsername(client: PoolClient, username: string) {
+    const query = `
+    SELECT 
+      u.id,
+      u.login,
+      u.password_hash  
+    FROM "User" u
+    WHERE u.login = $1
+  `;
+
+    const result = await client.query(query, [username]);
+    //console.log(result)
+    return result.rows[0] ?? null;
+  }
+}
+
