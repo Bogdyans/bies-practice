69682d1d2e19e2e22c6fe386e457e1766237b676
CapEugene <evgexa_2003@bk.ru>
2025-05-12 21:30:54 +0300

    feat(admin): add organization creation functionality

---
+++ src/app/api/organizations/route.ts
@@ -0,0 +1,31 @@
+import OrganizationController from "@/controllers/organization";
+import Jwt from "@/lib/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const decoded = await Jwt.getDecoded(request);
+  if (!decoded || !("id" in decoded)) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+  }
+
+  const formData = await request.formData();
+
+  const name = formData.get("name") ? String(formData.get("name")) : null;
+
+  if (!name) {
+    return NextResponse.json(
+      { error: "Missing required fields" },
+      { status: 500 }
+    );
+  }
+
+  try {
+    const organization = await OrganizationController.createOrganization(name);
+    return NextResponse.json({ organization }, { status: 200 });
+  } catch {
+    return NextResponse.json(
+      { error: "Error creating organization" },
+      { status: 500 }
+    );
+  }
+}
--- src/models/organization.ts
+++ src/models/organization.ts
@@ -1,14 +1,32 @@
 import { PoolClient } from "pg";
 
 export default class OrganizationModel {
-    static async fetchOrganizations(client: PoolClient) {
-        const query = `SELECT * FROM Organizations`;
+  static async fetchOrganizations(client: PoolClient) {
+    const query = `SELECT * FROM Organizations`;
 
-        try {
-            const result = await client.query(query);
-            return result.rows ?? null;
-        } catch (error) {
-            throw error;
-        }
+    try {
+      const result = await client.query(query);
+      return result.rows ?? null;
+    } catch (error) {
+      throw error;
     }
+  }
+
+  static async createOrganization(client: PoolClient, name: string) {
+    const insertQuery = `INSERT INTO organizations (name) VALUES ($1) RETURNING id`;
+    const checkQuery = `SELECT id FROM organizations WHERE name = $1`;
+
+    try {
+      const checkResult = await client.query(checkQuery, [name]);
+
+      if (checkResult.rows && checkResult.rows.length > 0) {
+        throw new Error("Данная организация уже существует");
+      }
+
+      const result = await client.query(insertQuery, [name]);
+      return result.rows[0] ?? null;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
